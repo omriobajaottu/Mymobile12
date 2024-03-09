d979d9ba5a987a6ff3e14f2d82486ed5d5b05847
ino ottu <inoottu@inos-macbook-pro.local>
2024-03-09 21:12:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mymobile/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mymobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val btnForgotPassword = findViewById<Button>(R.id.btnForgotPassword)
+        val btnShare = findViewById<Button>(R.id.btnShare) // Menambahkan tombol "Bagikan"
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        // Memulai intent implisit untuk membuka tautan YouTube
+
+        btnShare.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, "hallo.")
+            }
+            startActivity(Intent.createChooser(shareIntent, "Bagikan melalui"))
+        }
+    }
+}
